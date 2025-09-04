6c14f4058f51085ecda5c4e5ea30c28c6c6ff074
Yury Mediro <sagn375@gmail.com>
2025-09-04 17:16:18 +0300

    feat: add CartContext

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,17 @@
 import { HomePage } from '@/pages/home/HomePage'
 import './App.css'
 import { Box, Container } from '@chakra-ui/react'
+import { CartProvider } from '@/entities/cart/CartContext'
 
 function App() {
 	return (
-		<Box minH='100h' bg='gray.100' py={8}>
-			<Container centerContent >
-				<HomePage />
-			</Container>
-		</Box>
+		<CartProvider>
+			<Box minH='100h' bg='gray.100' py={8}>
+				<Container centerContent>
+					<HomePage />
+				</Container>
+			</Box>
+		</CartProvider>
 	)
 }
 
---
+++ src/entities/cart/CartContext.tsx
@@ -0,0 +1,24 @@
+import type { CartItem, Pizza } from '@/shared/types/pizza'
+import { createContext, useContext } from 'react'
+import { useCart } from './useCart'
+
+type CartContextType = {
+	cart: CartItem[]
+	addToCart: (pizza: Pizza, selectedIngredients: string[]) => void
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined)
+
+export const useCartContext = () => {
+	const context = useContext(CartContext)
+	if (!context) {
+		throw new Error('useCartProvider must be used within a CartProvider')
+	}
+	return context
+}
+
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+	const cart = useCart()
+
+	return <CartContext.Provider value={cart}>{children}</CartContext.Provider>
+}
