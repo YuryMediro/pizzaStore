c88925288f04bb923c0caa440dcfd4daac67934c
Yury Mediro <sagn375@gmail.com>
2025-09-08 15:55:20 +0300

    Adding dist

--- src/shared/lib/calculateItemPrice.ts
+++
@@ -1,9 +0,0 @@
-import type { CartItem } from "../types/pizza"
-
-export const calculateItemPrice = (item: CartItem) => {
-	const ingredientsPrice = item.selectedIngredients.reduce((sum, ingId) => {
-		const ingredient = item.ingredients.find(ing => ing.id === ingId)
-		return sum + (ingredient?.price || 0)
-	}, 0)
-	return (item.basePrice + ingredientsPrice) * item.quantity
-}
--- src/shared/lib/steps.ts
+++
@@ -1,5 +0,0 @@
-export const steps = [
-	{ title: 'Корзина', description: 'Содержимое вашей корзины:' },
-	{ title: 'Данные', description: 'Данные для доставки' },
-	{ title: 'Подтверждение', description: 'Подтверждение заказа' },
-]
